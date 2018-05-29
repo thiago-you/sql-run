5a0dd19981c66d08c813ea81aa6e14f0dae09fb8
Thiago You <thya9o@outlook.com>
2018-05-29 08:51:32 -0300

    Improving query log

--- index.php
+++ index.php
@@ -4,6 +4,9 @@
     
     $sql = file_get_contents('query.sql');
     
+    // remove exec time
+    set_time_limit(0);
+    
     foreach ($databases as $database) {
         try {
             $connection->exec("USE {$database};");
@@ -12,18 +15,23 @@
             if (!$connection->exec($sql)) {
                 throw new \Exception("Error");   
             }
+            
+            echo "SQL Query on database <b>\"{$database}\"</b> successfully! <br>";
+            
         } catch (\Exception $e) {
             // save error
             $connection->exec("USE {$database_log};");
             $message = utf8_encode(str_replace("'", "\'", $e->getMessage()));
             $sql = utf8_encode(str_replace("'", "\'", $sql));
-            $connection->exec("INSERT INTO {$table_log} VALUES(NULL, '{$database}', '{$message}', 'teste', '{$sql}');"); 
+            $connection->exec("INSERT INTO {$table_log} VALUES(NULL, '{$database}', '{$message}', 'teste', '{$sql}');");
+            echo "SQL Query on database <b>\"{$database}\"</b> fail... <br>";
         } catch (PDOException$e) {
             // save error
             $connection->exec("USE {$database_log};");
             $message = utf8_encode(str_replace("'", "\'", $e->getMessage()));
             $sql = utf8_encode(str_replace("'", "\'", $sql));
             $connection->exec("INSERT INTO {$table_log} VALUES(NULL, '{$database}', '{$message}', 'teste', '{$sql}');");
+            echo "SQL Query on database <b>\"{$database}\"</b> fail... <br>";
         }
     }
     
